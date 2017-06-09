b3b14328e84d58e9627bf14f9aa681dd0a66a876
rpcach <rpcach@users.noreply.github.com>
2017-06-09 00:19:10 -0400

    main: spacing

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ library(jsonlite)
 source('R/getBoxscoreTraditional.R')
 
 # loads data
-player <- readRDS('data/season/player.rds')
+player <- readRDS('data/season/player.rds') 
 team <- readRDS('data/season/team.rds')
 starterBench <- readRDS('data/season/starter-bench.rds')
 

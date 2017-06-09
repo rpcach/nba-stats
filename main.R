79ff594bf3a8d4eb8acdab35dd9643ef90bf63b0
rpcach <rpcach@users.noreply.github.com>
2017-06-08 21:14:05 -0400

    created main.R

---
+++ main.R
@@ -0,0 +1,5 @@
+# loads data
+boxscore <- readRDS('data/season/boxscore.rds')
+boxscore_starter_bench <- readRDS('data/season/boxscore-starter-bench.rds')
+boxscore_team <- readRDS('data/season/boxscore-team.rds')
+

b414e294e2424d90f27a1c22e3893e9d0487c379
rpcach <rpcach@users.noreply.github.com>
2017-06-08 22:23:07 -0400

    created getBoxscoreTraditional.R

---
+++ R/getBoxscoreTraditional.R
@@ -0,0 +1,26 @@
+getBoxscoreTraditional <- function(game_id) {
+	url <- paste('http://stats.nba.com/stats/boxscoretraditionalv2?GameID=', game_id, '&RangeType=0&StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0',sep='')
+	json <- readLines(url)
+	data <- fromJSON(json)
+	data <- data[[3]]
+
+	temp <- data[data$name == 'PlayerStats', ]
+	PlayerStats <- temp$rowSet
+	PlayerStats <- as.data.frame(PlayerStats)
+	colnames(PlayerStats)<- unlist(temp$headers)
+
+	temp <- data[data$name == 'TeamStats', ]
+	TeamStats <- temp$rowSet
+	TeamStats <- as.data.frame(TeamStats)
+	colnames(TeamStats)<- unlist(temp$headers)
+
+	temp <- data[data$name == 'TeamStearterBenchStats',]
+	TeamStarterBenchStats <- temp$rowSet
+	TeamStarterBenchStats <- as.data.frame(TeamStarterBenchStats)
+	colnames(TeamStarterBenchStats)<- unlist(temp$headers)
+	
+	stats <- list(PlayerStats, TeamStats, TeamStarterBenchStats)
+	names(stats) <- c('player', 'team', 'starterBench')
+
+	return(stats)
+}

0cfc8590453dbec86a5b6bef72b63d6f91dcb73a
rpcach <rpcach@users.noreply.github.com>
2017-06-08 23:06:01 -0400

    main: loads functions

--- main.R
+++ main.R
@@ -1,7 +1,9 @@
 library(jsonlite)
 
+source('R/getBoxscoreTraditional.R')
+
 # loads data
 player <- readRDS('data/season/player.rds')
-starterBench <- readRDS('data/season/starter-bench.rds')
 team <- readRDS('data/season/team.rds')
+starterBench <- readRDS('data/season/starter-bench.rds')
 

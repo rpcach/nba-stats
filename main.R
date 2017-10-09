41f7cbdb1fcd593d81364449863bf9cc0c3a9757
rpcach <rpcach@users.noreply.github.com>
2017-10-09 16:34:12 -0400

    working on getting game dates

--- main.R
+++ main.R
@@ -1,5 +1,7 @@
-library(jsonlite)
+library(jsonlite) #used in getBoxscoreTraditional
+library(rvest) #used in getBoxscoreTraditional - actually no?
 
+setwd('nba-stats')
 source('R/getBoxscoreTraditional.R')
 
 # loads season data
@@ -12,3 +14,12 @@ player2 <- readRDS('data/playoffs/player.rds')
 team2 <- readRDS('data/playoffs/team.rds')
 starterBench2 <- readRDS('data/playoffs/starter-bench.rds')
 
+#goals:
+#add in scraping of date for each game
+
+game_id <- '0021500001'
+url <- paste('http://stats.nba.com/stats/boxscoresummaryv2?GameID=',game_id,sep='')
+x <- readLines(url)
+x <- fromJSON(x)
+x <- x$resultSets
+x[,3][[5]][1,1]

d00cc9e473793eb888768b43e9b46665dff01e1a
rpcach <rpcach@users.noreply.github.com>
2017-10-13 18:46:34 -0400

    added code to get game ids

--- main.R
+++ main.R
@@ -5,7 +5,7 @@ setwd('nba-stats')
 source('R/getBoxscoreTraditional.R')
 
 # loads season data
-player <- readRDS('data/season/player.rds') 
+player <- readRDS('data/season/player.rds')
 team <- readRDS('data/season/team.rds')
 starterBench <- readRDS('data/season/starter-bench.rds')
 
@@ -23,3 +23,26 @@ x <- readLines(url)
 x <- fromJSON(x)
 x <- x$resultSets
 x[,3][[5]][1,1]
+
+#columbus day
+setwd('nba-stats')
+library(jsonlite) #used in getBoxscoreTraditional
+#load data
+game_ids <- as.character(unique(player$GAME_ID))
+game_ids <- as.data.frame(cbind(game_ids,0), stringsAsFactors=FALSE)
+colnames(game_ids)[2] <- 'date'
+game_id <- game_ids$game_ids
+for(i in 37714:nrow(game_ids)) {
+	print(i)
+	url <- paste('http://stats.nba.com/stats/boxscoresummaryv2?GameID=',game_id[i],sep='')
+	x <- readLines(url)
+	x <- fromJSON(x)
+	x <- x$resultSets
+	game_ids$date[i] <- x[,3][[5]][1,1]
+
+	#game_ids$date[i] <- fromJSON(readLines(paste('http://stats.nba.com/stats/boxscoresummaryv2?GameID=',game_ids$game_ids[i],sep='')))$resultSets[,3][[5]][1,1]
+
+	#Sys.sleep(1)
+}
+save(game_ids, file='game_ids_COMPLETE.rds')
+# 38,367 rows

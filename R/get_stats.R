c29be83569bffb757e14f765561e89d60a3b3102
rpcach <rpcach@users.noreply.github.com>
2017-10-18 20:22:02 -0400

    get_stats.R now handles multiple players and years at a time. Can also show NULL rows for players (years which player did not play in). Can also enable greater precision on statistics. (mantissa = # digits after decimal point)

--- R/get_stats.R
+++ R/get_stats.R
@@ -1,35 +1,44 @@
-get_stats <- function(p, name, year=2016) {
-	y <- paste('2', substr(year,3,4), sep='')
-	p <- p[p$player_name == name & substr(p$game_id,1,3) == y, ]
+get_stats <- function(p, names, years=2016, mantissa=2, hide_null=TRUE) {
+	p <- p[p$player_name %in% names & substr(p$game_id,2,3) %in% substr(years,3,4), ]
 
-	stats <- NULL
+	stats <- matrix(0, ncol=18, nrow=0)
 
-	stats <- c(stats,
-		unique(as.character(p$player_name)),
-		year,
-		mean(p$time[p$time != 0]),
-		sum(p$fgm, na.rm=TRUE),
-		sum(p$fga, na.rm=TRUE),
-		sum(p$fgm, na.rm=TRUE) / sum(p$fga, na.rm=TRUE),
-		sum(p$fg3m, na.rm=TRUE),
-		sum(p$fg3a, na.rm=TRUE),
-		sum(p$fg3m, na.rm=TRUE) / sum(p$fg3a, na.rm=TRUE),
-		sum(p$ftm, na.rm=TRUE),
-		sum(p$fta, na.rm=TRUE),
-		sum(p$ftm, na.rm=TRUE) / sum(p$fta, na.rm=TRUE),
-		mean(p$reb, na.rm=TRUE),
-		mean(p$ast, na.rm=TRUE),
-		mean(p$stl, na.rm=TRUE),
-		mean(p$blk, na.rm=TRUE),
-		mean(p$to, na.rm=TRUE),
-		mean(p$pts, na.rm=TRUE))
-	stats <- rbind(stats)
+	for(i in names) {
+		if(hide_null & !(i %in% p$player_name)) { next }
+		p2 <- p[p$player_name == i, ]
+		for(j in 1:length(years)) {
+			if(!hide_null & !(substr(years[j],3,4) %in% substr(p2$game_id,2,3))) { 
+				stats <- rbind(stats, c(i,years[j],0,0,0,NaN,0,0,NaN,0,0,NaN,0,0,0,0,0,0))
+				next
+			}
+			p3 <- p2[substr(p2$game_id,2,3) == substr(years[j],3,4), ]
+
+			stats <- rbind(stats, c(
+				i,
+				years[j],
+				mean(p3$time[p3$time != 0]),
+				sum(p3$fgm, na.rm=TRUE),
+				sum(p3$fga, na.rm=TRUE),
+				sum(p3$fgm, na.rm=TRUE) / sum(p3$fga, na.rm=TRUE),
+				sum(p3$fg3m, na.rm=TRUE),
+				sum(p3$fg3a, na.rm=TRUE),
+				sum(p3$fg3m, na.rm=TRUE) / sum(p3$fg3a, na.rm=TRUE),
+				sum(p3$ftm, na.rm=TRUE),
+				sum(p3$fta, na.rm=TRUE),
+				sum(p3$ftm, na.rm=TRUE) / sum(p3$fta, na.rm=TRUE),
+				mean(p3$reb, na.rm=TRUE),
+				mean(p3$ast, na.rm=TRUE),
+				mean(p3$stl, na.rm=TRUE),
+				mean(p3$blk, na.rm=TRUE),
+				mean(p3$to, na.rm=TRUE),
+				mean(p3$pts, na.rm=TRUE)))
+		}
+	}
 	stats <- as.data.frame(stats, stringsAsFactors=FALSE)
 	for(i in 2:ncol(stats)) { stats[, i] <- as.numeric(stats[, i]) }
 
 	colnames(stats) <- c(colnames(p)[6],'season',colnames(p)[c(8,11:25)])
-	stats[1, 2:ncol(stats)] <- round(stats[1 , 2:ncol(stats)], digits=2)
-	row.names(stats) <- NULL
+	stats[, 2:ncol(stats)] <- round(stats[ , 2:ncol(stats)], digits=mantissa)
 
 	return(stats)
 }

82139867fd6dd57c92b95675ac1b014e2de39575
rpcach <rpcach@users.noreply.github.com>
2017-11-13 15:44:42 -0500

    edited num_games.R

--- R/num_games.R
+++ R/num_games.R
@@ -1,9 +1,9 @@
 # returns the # of games the player has in the date range, assuming he did not switch teams since his last game
 
-num_games <- function(player, p, schedule, start, end) {
+num_games <- function(player, p, schedule, start, end=as.Date(start)+6) {
 	p <- p[p$player_name == player, ]
 	p <- p[order(p$game_id, decreasing=TRUE), ]
-	team <- p$team_abbreviation[1]
+	team <- as.character(p$team_abbreviation[1])
 
 	schedule$date_time <- as.Date(schedule$date_time)
 	schedule <- schedule[schedule$date_time >= start & schedule$date_time < as.Date(end)+1, ]
@@ -11,3 +11,4 @@ num_games <- function(player, p, schedule, start, end) {
 
 	return(nrow(schedule))
 }
+

ca5130935f581613c36596802ae9c7416fc16e18
rpcach <rpcach@users.noreply.github.com>
2017-11-13 15:11:19 -0500

    small edits

--- R/updateCurrentSeasonData.R
+++ R/updateCurrentSeasonData.R
@@ -1,12 +1,18 @@
 updateCurrentSeasonData <- function() {
 	player <- readRDS('data/raw/player.rds')
+	team <- readRDS('data/raw/team.rds')
+	starterBench <- readRDS('data/raw/starter-bench.rds')
 
-	player <- player[substr(player$GAME_ID,3,5) == 217, ]
-	game_ids_to_skip <- unique(player$GAME_ID)
+	p2 <- player[substr(player$GAME_ID,3,5) == 217, ]
+	game_ids_to_skip <- unique(p2$GAME_ID)
 
 	if(!length(game_ids_to_skip)) {
 		game_ids_to_skip <- NULL
 	}
-	
-	return(getSeasonData(2017, player$GAME_ID))
+
+	new_data <- getSeasonData(2017, game_ids_to_skip)
+
+	saveRDS(rbind(new_data[[1]], player), 'data/raw/player.rds')
+	saveRDS(rbind(new_data[[2]], team), 'data/raw/team.rds')
+	saveRDS(rbind(new_data[[3]], starterBench), 'data/raw/starter-bench.rds')
 }

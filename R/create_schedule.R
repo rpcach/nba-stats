334417a12a593558eb3e2abe37a0bc02c6bc0854
rpcach <rpcach@users.noreply.github.com>
2017-11-20 15:28:00 -0500

    turned get_team_names.R and create_schedule into functions

--- R/create_schedule.R
+++ R/create_schedule.R
@@ -1,53 +1,58 @@
 library(openxlsx)
-source('R/team_names.R')
 
-# download xls files from basketball-reference.com
-# convert xlsx files to xlsx files
-# rename them with the following format "nba-MONTH.xlsx"
-files <- list(read.xlsx('D:/downloads/nba-oct.xlsx'))
-files <- c(files, list(read.xlsx('D:/downloads/nba-nov.xlsx')))
-files <- c(files, list(read.xlsx('D:/downloads/nba-dec.xlsx')))
-files <- c(files, list(read.xlsx('D:/downloads/nba-jan.xlsx')))
-files <- c(files, list(read.xlsx('D:/downloads/nba-feb.xlsx')))
-files <- c(files, list(read.xlsx('D:/downloads/nba-mar.xlsx')))
-files <- c(files, list(read.xlsx('D:/downloads/nba-apr.xlsx')))
+create_schedule <- function() {
+	team_names <- get_team_names()
 
-months <- c(10,11,12,1,2,3,4)
+	# 1. download xls files from basketball-reference.com
+	# 2. convert xlsx files to xlsx files
+	# 3. rename them with the following format "nba-MONTH.xlsx"
+	files <- list(read.xlsx('schedules/nba-oct.xlsx'))
+	files <- c(files, list(read.xlsx('schedules//nba-nov.xlsx')))
+	files <- c(files, list(read.xlsx('schedules/nba-dec.xlsx')))
+	files <- c(files, list(read.xlsx('schedules/nba-jan.xlsx')))
+	files <- c(files, list(read.xlsx('schedules/nba-feb.xlsx')))
+	files <- c(files, list(read.xlsx('schedules/nba-mar.xlsx')))
+	files <- c(files, list(read.xlsx('schedules/nba-apr.xlsx')))
 
-for(i in 1:length(files)) {
-	df <- files[[i]]
+	months <- c(10,11,12,1,2,3,4)
 
-	dates <- strsplit(df$Date, ', ')
-	dates <- unlist(dates)
-	dates <- dates[seq(2, length(dates), 3)]
-	dates<- strsplit(dates,' ')
-	dates <- unlist(dates)
-	dates <- dates[seq(2, length(dates), 2)]
-	dates <-as.numeric(dates)
+	for(i in 1:length(files)) {
+		df <- files[[i]]
 
-	ifelse(i <= 3, year <- 2017, year <- 2018)
-	dates <- as.Date(paste(year, months[i], dates, sep='-'))
+		dates <- strsplit(df$Date, ', ')
+		dates <- unlist(dates)
+		dates <- dates[seq(2, length(dates), 3)]
+		dates<- strsplit(dates,' ')
+		dates <- unlist(dates)
+		dates <- dates[seq(2, length(dates), 2)]
+		dates <-as.numeric(dates)
 
-	df$Date <- dates
+		ifelse(i <= 3, year <- 2017, year <- 2018)
+		dates <- as.Date(paste(year, months[i], dates, sep='-'))
 
-	for(j in 1:nrow(df)) {
-		df$'Visitor/Neutral'[j] <- team_names$abbrev[team_names$name == df$'Visitor/Neutral'[j]]
-		df$'Home/Neutral'[j] <- team_names$abbrev[team_names$name == df$'Home/Neutral'[j]]
+		df$Date <- dates
+
+		for(j in 1:nrow(df)) {
+			df$'Visitor/Neutral'[j] <- team_names$abbrev[team_names$name == df$'Visitor/Neutral'[j]]
+			df$'Home/Neutral'[j] <- team_names$abbrev[team_names$name == df$'Home/Neutral'[j]]
+		}
+
+		df$time <- df$'Start.(ET)' * 24
+		df$hours <- df$time %/% 1
+		df$mins <- df$time %% 1 * 60
+
+		df$dates <- as.POSIXlt(paste(df$Date,' ',df$hours,':',df$mins,':00',sep=''), tz='US/Eastern')
+
+		df <- data.frame(df$dates, df$'Visitor/Neutral', df$'Home/Neutral')
+		colnames(df) <- c('date_time','away','home')
+
+		files[[i]] <- df
 	}
 
-	df$time <- df$'Start.(ET)' * 24
-	df$hours <- df$time %/% 1
-	df$mins <- df$time %% 1 * 60
+	schedule <- files[[1]]
+	for(i in 2:length(files)) {
+		schedule <- rbind(schedule, files[[i]])
+	}
 
-	df$dates <- as.POSIXlt(paste(df$Date,' ',df$hours,':',df$mins,':00',sep=''), tz='US/Eastern')
-
-	df <- data.frame(df$dates, df$'Visitor/Neutral', df$'Home/Neutral')
-	colnames(df) <- c('date_time','away','home')
-
-	files[[i]] <- df
-}
-
-schedule <- files[[1]]
-for(i in 2:length(files)) {
-	schedule <- rbind(schedule, files[[i]])
+	write.csv(schedule, 'schedule.csv', row.names=FALSE)
 }

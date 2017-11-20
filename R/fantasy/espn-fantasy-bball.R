1d095d1a27c52d9a1fa763d688aa10e16eae70d6
rpcach <rpcach@users.noreply.github.com>
2017-11-20 17:53:06 -0500

    reverted get_stats.R back to taking in multiple player names

--- R/fantasy/espn-fantasy-bball.R
+++ R/fantasy/espn-fantasy-bball.R
@@ -1,6 +1,6 @@
 library(rvest)
 
-roster_stats <- function(url) {
+roster_stats <- function(url, year=2017) {
 
 	roster <- url %>% read_html() %>% html_nodes(xpath='//*[@id="playertable_0"]') %>% html_table()
 	roster <- roster[[1]]
@@ -8,7 +8,8 @@ roster_stats <- function(url) {
 
 	roster <- roster[-(which(roster %in% c('STARTERS','PLAYER, TEAM POS','','BENCH')))]
 
-	roster <- gsub(',.*$','', roster)
+	roster <- gsub(',.*$', '', roster)
+	roster <- gsub('[*]', '', roster)
 
-	get_stats(p, roster, 2017)
+	stats <- get_stats(roster, year)
 }
